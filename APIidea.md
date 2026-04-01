1591b6c1d5d0ac388b6a10b8367086f00c07ff92
Jaccob Bagwell <jaccob@gondor.ctc.co>
2026-04-01 16:41:59 -0400

    got the API working for me and was able to pull data to the terminal

---
+++ APIidea.md
@@ -0,0 +1,8 @@
+might be able to make own API
+
+            ======IDEAS=======
+- could use espn boxscore to get informaiton
+-https://www.espn.com/college-football/boxscore/_/gameId/401110724 
+    - this is the link the number at the end is the game id. need to find a way to get that game id number for the week
+    - id number is speceific for that week
+    - webscraping to find the info
---
+++ CollegeFantasyFootballAPI.py
@@ -0,0 +1,66 @@
+
+import time
+import cfbd
+import json
+from cfbd.rest import ApiException
+from pprint import pprint
+
+
+#will tell what tier and how many call i have left
+def CallsLeft():
+    with cfbd.ApiClient(configuration) as api_client:
+        # Create an instance of the API class
+        api_instance = cfbd.InfoApi(api_client)
+
+        try:
+            api_response = api_instance.get_user_info()
+            print("The response of InfoApi->get_user_info:\n")
+            pprint(api_response)
+        except Exception as e:
+            print("Exception when calling InfoApi->get_user_info: %s\n" % e)
+
+
+
+# Defining the host is optional and defaults to https://api.collegefootballdata.com
+# See configuration.py for a list of all supported configuration parameters.
+configuration = cfbd.Configuration(
+    host = "https://api.collegefootballdata.com"
+)
+
+# The client must configure the authentication and authorization parameters
+# in accordance with the API server security policy.
+# Examples for each auth method are provided below, use the example that
+# satisfies your auth use case.
+
+# Configure Bearer authorization: apiKey
+configuration = cfbd.Configuration(
+    access_token = "ANwrLmh7bIKfnl70S1zZNm+NqYkdTc7jTH/7uEfjuBjGQHe/7nGDFEx/VE8gsFdC"
+
+)
+
+
+with cfbd.ApiClient(configuration) as api_client:
+    # Create an instance of the API class
+    api_instance = cfbd.GamesApi(api_client)
+    #parameters
+    year = 2022 # int | Optional year filter (optional)
+    team = 'michigan' # str | Optional team filter (optional)
+    week = 5
+    
+
+
+    try:
+        api_response = api_instance.get_games(year=year,week=week,team=team)
+        # Convert to readable JSON
+        readable = [coach.to_dict() for coach in api_response]
+        with open('output_file.txt', 'w') as text_file:
+            json.dump(readable, text_file, indent=4, default=str) #
+        #print(json.dumps(readable, indent=4, default=str))
+        
+    except Exception as e:
+        print("Exception when calling CoachesApi->get_coaches: %s\n" % e)
+
+
+CallsLeft()
+
+
